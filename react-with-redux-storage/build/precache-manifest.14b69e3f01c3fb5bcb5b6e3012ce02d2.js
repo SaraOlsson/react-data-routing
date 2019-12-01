06f67250a705a3b078ac7b0582eae981633b6a04
SaraOlsson <sarol688@student.liu.se>
2019-12-02 00:15:36 +0100

    same project but with redux data management

---
+++ react-with-redux-storage/build/precache-manifest.14b69e3f01c3fb5bcb5b6e3012ce02d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cc1c385aa7237ddc05f59d0e3a4004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b265c3df3c14147a971",
+    "url": "/static/css/main.0e3c6df8.chunk.css"
+  },
+  {
+    "revision": "c34b9b9054d28d29897c",
+    "url": "/static/js/2.d724307f.chunk.js"
+  },
+  {
+    "revision": "1b265c3df3c14147a971",
+    "url": "/static/js/main.dd6832cc.chunk.js"
+  },
+  {
+    "revision": "0c41fc7c811e1d1bb050",
+    "url": "/static/js/runtime-main.233114b3.js"
+  }
+]);
---
+++ react-with-redux-storage/src/pages/homepage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const HomePage = () => {
+
+  return (
+    <div>
+
+      <pre>{`
+            Welcome,
+
+            This sample app uses react-router-dom to navigate between pages
+            and react-redux with Hooks to handle data between components. Redux
+            is used in many real applications.
+
+            Navigation details:
+            See App.js for route definitions. The user is redirected with Link
+            components from the Menu component. Components corresponding to
+            whole pages are in the /pages folder and small components in the
+            /components folder and are imported where needed.
+
+            Redux details:
+            Changes to the create-react-app project follow the simple tutorial linked below.
+            You can keep your usual state hooks (e.g. const [name, setName] = React.useState('') )
+            as they are if they don't need to be reachable from other components. The basic concepts:
+            store.js contains definitions of global states and how they should be updated.
+            useDispatch is hook to change the state, see page1 and how the dispatch type and action
+            corresponds to the case "SETTITLE" and action object in store.js. Also see how dispatch
+            from page3 corresponds to the case "ADDUSER" in store.js.
+            useSelector is hook to access the state, see page2. The useSelector also subscribes
+            to the redux store, so the component will re-render if the state changes.
+
+
+            Other:
+            Functional components are used throughout the app
+            For style, the npm package @material-ui/core is used. Each component
+            has a style object created with makeStyles to keep the CSS straightforward
+
+          `}  <a href="https://github.com/SaraOlsson/react-with-local-storage/tree/master/react-with-redux-storage">Source code on GitHub</a>
+              {`\n            `}
+              <a href="https://reactgo.com/react-redux-hooks/">React Redux hooks tutorial with examples</a></pre>
+
+
+
+    </div>
+  );
+};
+
+export default HomePage;
